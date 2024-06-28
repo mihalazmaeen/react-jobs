69aae93242b9faa9c9bb7e6493b97e57ea3adfc2
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-06-28 16:08:38 +0600

    navbar, home card, card separated

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+
+
+import PropTypes from "prop-types";
+const Card = ({children, bg='bg-grey-100'}) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
+}
+
+Card.propTypes = {
+  children: PropTypes.node,
+  bg: PropTypes.string
+}
+
+export default Card
