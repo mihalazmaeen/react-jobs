6edfaf91fef3dbc08ea84b43b8a9d320cbce8cc7
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-06-30 13:54:44 +0600

    router, homepage separation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings"
-import Navbar from "./components/Navbar"
-import ViewAllJobs from "./components/ViewAllJobs"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+       <Route  index element={<HomePage />} />
+    </Route>
+   
+  )
+);
 
 // import React from 'react'
 
-
 const App = () => {
   return (
-   <>
-    <body>
-   
-    <Navbar/>
-    {/* <!-- Hero --> */}
-    <Hero/>
+   <RouterProvider router={router} />
+  );
+};
 
-    {/* <!-- Developers and Employers --> */}
-    <HomeCards/>
-
-    {/* <!-- Browse Jobs --> */}
-    <JobListings/>
-
-    <ViewAllJobs/>
-
-    
-  </body>   
-   </>
-  )
-}
-
-export default App
+export default App;
